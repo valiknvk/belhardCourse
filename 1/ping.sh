cc5a2dba18fdf82f2cda24d10bf8ab7184b7babb
Valik <48516714+valiknvk@users.noreply.github.com>
2023-08-14 15:56:46 +0300

    Update ping.sh

--- 1/ping.sh
+++ 1/ping.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 read -p "Enter ip or dns: " ip;
 if ping -q -W 1 -c 1 $ip > /dev/null 2>&1; then
-    echo "Connectivity OK" # stdout
+    echo "Connectivity is OK" # stdout
 else
-    >&2 echo "Connectivity not available" #stderr
+    >&2 echo "Connectivity is not available" #stderr
 fi
